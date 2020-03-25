21b33928e892e9dee19c2fbe632de9d2a10a7fb0
Lukas <lurxar@gmail.com>
2020-03-25 12:14:49 +0100

    made rocket

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+/*
+	this fct does this.
+*/
+
+var doCoolStuff = function(){
+	var currentClassName = document.getElementById('cool').className;
+	// chanfinf class-name
+	if(currentClassName == 'cool'){
+		document.getElementById('cool').className = 'cool red';
+	}else{
+		document.getElementById('cool').className = 'cool';
+	}
+}
+
 var sayMyName = function(name){
 	alert('My name is: ' + name);
 }
