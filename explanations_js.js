c0decd474442a8b7e1b787c5edec849c18400f8d
Lukas <lurxar@gmail.com>
2020-03-25 10:50:27 +0100

    combined html css and js

---
+++ explanations_js.js
@@ -0,0 +1,23 @@
+var favColor = 'red'; //favColor: CamelCase
+var myFavColors = ['blue','red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Lukas',
+	lastName: 'Lurxar',
+	favColor: ['blue','red','green'],
+	yearsAlive: 21;
+}
+
+function whatIsMyFavoriteColor(){
+
+}
+
+var whatIsMyFavoriteColor = function(){
+	return true;
+	return 'blue';
+}
+
+var doSomething = function(){
+	console.log('doSomething!!!');
+}
